aa8dd5860055b74b09b95b72356ecbe62b978104
ujjwal <ujjwalsrivastava500@gmail.com>
2023-05-11 10:57:15 +0530

    intro2

--- lib/main.dart
+++ lib/main.dart
@@ -14,44 +14,56 @@ class MyApp extends StatelessWidget {
       body: Container(
         child: Column(
           children: [
-            const CircleAvatar(
-              radius: 90.0,
-              backgroundImage: AssetImage('assets/images/retr.jpg'),
+            Padding(
+              padding: const EdgeInsets.all(38.0),
+              child: const CircleAvatar(
+                radius: 90.0,
+                backgroundImage: AssetImage('assets/images/retr.jpg'),
+              ),
             ),
-            Text(
-              "Raj Kumar Rao",
-              style: TextStyle(fontSize: 40, fontFamily: 'Pacifico'),
+            Padding(
+              padding: const EdgeInsets.all(0.0),
+              child: Text(
+                "Raj Kumar Rao",
+                style: TextStyle(fontSize: 40, fontFamily: 'Pacifico'),
+              ),
             ),
             Text(
               "I n d i a n    F i l m    A c t o r",
               style: TextStyle(fontSize: 22),
             ),
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.phone),
-                title: Text(
-                  "9696363656",
-                  style: TextStyle(fontSize: 25),
+            Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: Card(
+                child: ListTile(
+                  leading: Icon(Icons.phone),
+                  title: Text(
+                    "9696363656",
+                    style: TextStyle(fontSize: 25),
+                  ),
+                  subtitle: Text(
+                    "Phone Number",
+                    style: TextStyle(fontSize: 25),
+                  ),
+                  trailing: Icon(Icons.arrow_right),
                 ),
-                subtitle: Text(
-                  "Phone Number",
-                  style: TextStyle(fontSize: 25),
-                ),
-                trailing: Icon(Icons.arrow_right),
               ),
             ),
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.mail),
-                title: Text(
-                  "Alibaba420@gmail.com",
-                  style: TextStyle(fontSize: 23),
+            Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: Card(
+                child: ListTile(
+                  leading: Icon(Icons.mail),
+                  title: Text(
+                    "Alibaba420@gmail.com",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  subtitle: Text(
+                    "e-mail",
+                    style: TextStyle(fontSize: 25),
+                  ),
+                  trailing: Icon(Icons.arrow_right),
                 ),
-                subtitle: Text(
-                  "e-mail",
-                  style: TextStyle(fontSize: 25),
-                ),
-                trailing: Icon(Icons.arrow_right),
               ),
             )
           ],
